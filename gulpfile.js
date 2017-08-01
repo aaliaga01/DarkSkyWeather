b09a80734859c9ec6efc07eeab7269195181e1c0
aaliaga-casa <a.aliaga.val@gmail.com>
2017-08-01 18:40:34 -0300

    gulp instalado

--- gulpfile.js
+++ gulpfile.js
@@ -33,3 +33,5 @@ gulp.task('webserver', function(){
 gulp.task('watch', function(){
 	gulp.watch('assets/sass/*.scss', ['style']);
 })
+
+gulp.task('default',['script', 'style', 'webserver', 'watch']);
