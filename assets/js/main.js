56b376d35c45dd09004b5fb939d0cc0339ed863b
aaliaga-casa <a.aliaga.val@gmail.com>
2017-08-01 20:02:47 -0300

    error raro, odio ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,23 @@
 
 $(document).ready (function(){
 
-		$.ajax( {
-		url: "https://api.darksky.net/forecast/37457c3f51925aa523a0b4d7702c1e54/37.8267,-122.4233",
-		type: 'GET',
-		dataType: 'JSON',
-		})
-		.done(function(response) {
-			console.log("success");
-		})
-		.fail(function() {
-			console.log("error");
-		})
-		.always(function() {
-			console.log("complete");
-		});
+var cal= function(){
+
+	$.ajax( {
+	url: "https://api.darksky.net/forecast/37457c3f51925aa523a0b4d7702c1e54/37.8267,-122.4233",
+	type: 'GET',
+	dataType: 'JSON',
+	data: {'temperature' : 1},
+	})
+	.done(function(response) {
+		console.log("success");
+
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	}
+});
