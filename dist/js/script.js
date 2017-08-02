389f62208bdfaba2348c9ad29c7a481589ce9eb5
aaliaga-casa <a.aliaga.val@gmail.com>
2017-08-01 21:40:19 -0300

    otro intento

--- dist/js/script.js
+++ dist/js/script.js
@@ -10253,8 +10253,21 @@ return jQuery;
 } );
 
 
-$.getJSON('https://api.darksky.net/forecast/c8353c5e5293f515913acc8eb4aa2689/37.8267,-122.4233', function(resp) {
-    $.each(resp, function(k, v) {
-        console.log(k + ' : ' + v);
+
+$(document).ready(function() {
+
+    $.ajax({
+      url: 'https://api.darksky.net/forecast/c8353c5e5293f515913acc8eb4aa2689/37.8267,-122.4233',
+      type: 'GET',
+      dataType: 'json',
+      success: function(datos) {
+        $('#caja').text(JSON.stringify(datos));
+      }
+    });
 });
-});
+
+/* Me sale este error todo el rato, no se que hacer
+XMLHttpRequest cannot load https://api.darksky.net/forecast/c8353c5e5293f515913acc8eb4aa2689/37.8267,-122.4233. 
+No 'Access-Control-Allow-Origin' header is present on the requested resource. 
+Origin 'null' is therefore not allowed access.
+*/
